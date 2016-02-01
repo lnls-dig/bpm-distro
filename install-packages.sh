2d8eeeda588022c7f92d5d6535115eb765aa71f6
Lucas Russo <lerwys@gmail.com>
2016-02-01 20:28:05 -0200

    install-packages.sh: remove yum downloadonly
    We are going to rely on yum list installed
    command to do this

--- install-packages.sh
+++ install-packages.sh
@@ -3,22 +3,10 @@
 set -e
 set -x
 
-YUM_DOWNLOADONLY="--downloadonly --downloaddir=kickstart_build/all_rpms/"
-
 # Source ackages variables
 . ./packages.sh
 
-### Run everything "download only" first so we have all packages 
-### in a single place
-
-# Update database
-sudo yum update -y ${YUM_DONWLOADONLY}
-# Install CentOS development tools
-sudo yum groupinstall -y ${YUM_DONWLOADONLY} ${GROUPINSTALL_PACKAGES[@]} 
-# Install basic networking tools
-sudo yum install -y ${YUM_DONWLOADONLY} ${PACKAGES} 
-
-### Install everything normally
+### Install all packages
 
 # Update database
 sudo yum update -y 

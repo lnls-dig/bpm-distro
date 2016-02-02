de87caf856c96fa91d0b29a45c4c0dc111489ce2
Lucas Russo <lerwys@gmail.com>
2016-02-02 05:43:12 -0500

    setup-all: pass ISO path option via command-line to setup-iso-tree.sh

--- setup-all.sh
+++ setup-all.sh
@@ -3,6 +3,35 @@
 set -e
 set -x
 
+# Path of device containing Distribution (CentOS 7) install disk
+ISO_PATH=
+
+# Get command-line options
+while getopts ":i:" opt; do
+    case "${opt}" in
+        i)
+            ISO_PATH=${OPTARG}
+            ;;
+        \?) 
+            echo "Invalid option: $OPTARG" >&2
+            exit 1
+            ;;
+        :) 
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+# Check if argument was set
+if [ -z "${ISO_PATH}" ]; then
+    usage
+    exit 1;
+fi
+
+### Install packages, scripts and setup ISO tree
+
 # Install packages
 ./install-packages.sh
 
@@ -13,4 +42,4 @@ set -x
 ./setup-initscripts.sh
 
 # Setup ISO tree
-./setup-iso-tree.sh
+./setup-iso-tree.sh -i ${ISO_PATH}

986a90a2382fdae2f7240fcdb374fc3925c4d563
Lucas Russo <lerwys@gmail.com>
2016-02-04 10:02:41 -0500

    setup-repo.sh: change CentOS label to default one

--- setup-repo.sh
+++ setup-repo.sh
@@ -10,7 +10,7 @@ cd ../../
 
 # Build ISO
 cd kickstart_build
-chmod 644 isolinux/isolinux.bin
-mkisofs -o lnls-bpm-centos7.iso -b isolinux.bin -c boot.cat -no-emul-boot \
-    -V 'LNLS BPM CentOS 7 x86_64' \
+chmod 644 isolinux/isolinux/isolinux.bin
+mkisofs -o lnls-bpm-centos7.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
+    -V 'CentOS 7 x86_64' \
     -boot-load-size 4 -boot-info-table -R -J -v -T isolinux/

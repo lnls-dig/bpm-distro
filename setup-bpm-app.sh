e39e230fd90623c9e2e15f9535451a9adacfb112
Lucas Russo <lerwys@gmail.com>
2016-02-26 14:22:10 -0500

    setup-bpm-app.sh: add script to download BPM packages

---
+++ setup-bpm-app.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+set -x
+
+PWD=$(pwd)
+
+# Setup dirs
+mkdir -p kickstart_build/isolinux/postinstall/apps
+cd kickstart_build/isolinux/postinstall/apps
+
+# Clone BPM App repo
+git clone https://github.com/lnls-bpm/bpm-app
+
+# Download the repos, but don't install them
+cd bpm-app
+./get-all.sh -r server -b afcv3_1 -a yes -e yes -o
+
+cd ${PWD}

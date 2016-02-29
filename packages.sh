268aee9d289d2f48dd2274665b5bddc0646e332d
Lucas Russo <lerwys@gmail.com>
2016-02-29 07:39:39 -0500

    pacakges.sh: add EPICS dependencies pacakges

--- packages.sh
+++ packages.sh
@@ -10,8 +10,12 @@ GROUPINSTALL_PACKAGES+=('Minimal Install')
 GROUPINSTALL_PACKAGES+=('Development Tools')
 
 PACKAGES="net-tools vim git make automake autoconf libtool pkgconfig \
-binutils readline perl gcc-c++ python wget glib2 glib2-devel \
+binutils readline perl gcc gcc-c++ python wget glib2 glib2-devel \
 uuid-devel readline-devel libusb-devel bzip2-devel libxml2-devel \
 perl-XML-Simple selinux-policy yum-utils openssl libdb-utils createrepo \
 genisoimage pykickstart kernel kernel-tools kernel-tools-libs kernel-headers \
-ntp ntpdate acpid"
+ntp ntpdate acpid libusb-devel "
+
+# EPICS dependencies
+PACKAGES+="libstdc++-devel lapack openmotif-devel fontconfig-devel \
+libjpeg-turbo-devel libtiff-devel giflib-devel libcurl-devel flex"

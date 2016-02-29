b5554fd1e0b6ea689425da1bbfd5a6dce86ebd1c
Lucas Russo <lerwys@gmail.com>
2016-02-29 08:46:39 -0500

    install-re2c.sh: remove system re2c package before installing our version

--- install-re2c.sh
+++ install-re2c.sh
@@ -8,5 +8,8 @@ RE2C_VERSION=0.13.5-1
 # Download RPM
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/re2c-${RE2C_VERSION}.el6.x86_64.rpm
 
+# Remove re2c that may be already installed
+sudo yum remove re2c
+
 # Install it
 sudo rpm -Uvh --replacepkgs re2c-${RE2C_VERSION}.el6.x86_64.rpm

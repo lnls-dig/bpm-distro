8cad565bd3fd8bba327dbed9e6e30139bf30ea04
Lucas Russo <lerwys@gmail.com>
2016-02-29 08:34:51 -0500

    install-re2c.sh: change re2c repo address to a more standard one

--- install-re2c.sh
+++ install-re2c.sh
@@ -6,7 +6,7 @@ set -x
 RE2C_VERSION=0.13.5-1
 
 # Download RPM
-wget pkgs.repoforge.org/re2c/re2c-${RE2C_VERSION}.el6.rf.x86_64.rpm
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/re2c-${RE2C_VERSION}.el6.x86_64.rpm
 
 # Install it
-sudo rpm -Uvh --replacepkgs re2c-${RE2C_VERSION}*.rpm
+sudo rpm -Uvh --replacepkgs re2c-${RE2C_VERSION}.el6.x86_64.rpm

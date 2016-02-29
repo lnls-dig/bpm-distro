d7acb29d564397d7853be9956ef8269cd14904ac
Lucas Russo <lerwys@gmail.com>
2016-02-29 09:03:36 -0500

    install-re2c.sh,setup-iso-tree.sh: install re2c package separately
    This is necessary as re2c is not available through
    the official RHEL7 repositories at the moment.
    
    So, we download a RHEL6 re2c package and include it
    in the build manually by:
    
    1) moving the RPM to all_rpms folder and
    2) by specifying it in comps.xml file

--- install-re2c.sh
+++ install-re2c.sh
@@ -5,11 +5,16 @@ set -x
 
 RE2C_VERSION=0.13.5-1
 
+# Change to RPM directory
+cd kickstart_build/all_rpms
+
 # Download RPM
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/re2c-${RE2C_VERSION}.el6.x86_64.rpm
 
 # Remove re2c that may be already installed
-sudo yum remove re2c
+sudo yum remove -y re2c
 
 # Install it
 sudo rpm -Uvh --replacepkgs re2c-${RE2C_VERSION}.el6.x86_64.rpm
+
+cd ../../
--- setup-iso-tree.sh
+++ setup-iso-tree.sh
@@ -112,8 +112,8 @@ sudo chown -R ${USER}:${USER} kickstart_build/all_rpms/
 # Download oll of our installed packages
 cd kickstart_build/all_rpms/
 rpm -qa | \
-# Exclude gpg-pubkey* fake packages
-sed -e "s/^gpg-pubkey.*//g" > \
+# Exclude gpg-pubkey* fake and re2c (installed separately) packages
+sed -e 's/^gpg-pubkey.*//g' -e 's/^re2c.*//g' > \
 installed_packages; while read -r package; do COMMAND="sudo yumdownloader --resolve ${package}"; echo "${COMMAND}"; eval ${COMMAND}; done < installed_packages
 rm installed_packages
 cd ../../

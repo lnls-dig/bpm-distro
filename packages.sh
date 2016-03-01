e2d61acbe550ccc8e98a005010274f14151c421d
Lucas Russo <lerwys@gmail.com>
2016-03-01 08:14:20 -0500

    packages.sh,comps.xml: add more missing packages for EPICS

--- packages.sh
+++ packages.sh
@@ -11,8 +11,8 @@ GROUPINSTALL_PACKAGES+=('Development Tools')
 
 PACKAGES="net-tools vim git make automake autoconf libtool pkgconfig \
 binutils readline perl gcc gcc-c++ python wget glib2 glib2-devel \
-uuid-devel readline-devel libusb-devel bzip2-devel libxml2-devel \
-perl-XML-Simple selinux-policy yum-utils openssl libdb-utils createrepo \
+uuid-devel readline-devel libusb-devel bzip2-devel libpng12-devel libxml2-devel \
+ncurses-devel perl-XML-Simple selinux-policy yum-utils openssl libdb-utils createrepo \
 genisoimage pykickstart kernel kernel-tools kernel-tools-libs kernel-headers \
 ntp ntpdate acpid libusb-devel bash-completion perl-ExtUtils-* perl-Pod-Checker \
 perl-Pod-Parser "

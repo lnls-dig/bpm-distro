63a8107c3f6fef0db05b90158e6133c13d004158
Lucas Russo <lerwys@gmail.com>
2016-03-09 23:09:59 -0300

    install-xorriso.sh: install xorriso package and its deps
    This package is not available through official repos,
    so we use a older one

---
+++ install-xorriso.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+set -x
+
+XORRISO_VERSION=1.4.2-1
+LIBBURN_VERSION=1.4.2-2
+LIBISOBURN_VERSION=1.4.2-1
+LIBISOFS_VERSION=1.4.2-1
+
+# Change to RPM directory
+cd kickstart_build/all_rpms
+
+# Download Repoforge
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/libisofs-${LIBISOFS_VERSION}.el5.x86_64.rpm
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/libburn-${LIBBURN_VERSION}.el5.x86_64.rpm
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/libisoburn-${LIBISOBURN_VERSION}.el5.x86_64.rpm
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/xorriso-${XORRISO_VERSION}.el5.x86_64.rpm
+
+# Remove xorriso that may be already installed
+sudo yum remove -y libisofs
+sudo yum remove -y libburn
+sudo yum remove -y libisoburn
+sudo yum remove -y xorriso
+
+# Install it
+sudo rpm -Uvh --replacepkgs libisofs-${LIBISOFS_VERSION}.el5.x86_64.rpm
+sudo rpm -Uvh --replacepkgs libburn-${LIBBURN_VERSION}.el5.x86_64.rpm
+sudo rpm -Uvh --replacepkgs libisoburn-${LIBISOBURN_VERSION}.el5.x86_64.rpm
+sudo rpm -Uvh --replacepkgs xorriso-${XORRISO_VERSION}.el5.x86_64.rpm
+
+cd ../../

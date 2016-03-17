c200c3224fc7915171575a503e53ccbc6bdf3b08
Lucas Russo <lerwys@gmail.com>
2016-03-17 08:10:23 -0400

    cleanup-oldkernels.sh: say "yes" by default for removing old kernels

--- cleanup-oldkernels.sh
+++ cleanup-oldkernels.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Leave just the most recent kernel
-sudo package-cleanup --oldkernels --count=1
+sudo package-cleanup -y --oldkernels --count=1
 
 # Make sure our all_rpms and Packages folder do not contain
 # any kernel pacakges

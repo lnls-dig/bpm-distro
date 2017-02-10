94313b8a89d37ed5a2ae526e0b3b1cb35aa6cf62
Lucas Russo <lerwys@gmail.com>
2017-02-10 15:17:36 -0500

    {comps.xml,packages.sh}: add missing firewall packages

--- packages.sh
+++ packages.sh
@@ -18,6 +18,7 @@ ncurses-devel perl-XML-Simple selinux-policy yum-utils openssl libdb-utils creat
 genisoimage pykickstart ntp ntpdate acpid libusb-devel bash-completion \
 perl-ExtUtils-* perl-Pod-Checker perl-Pod-Parser iptables-services syslinux \
 pciutils telnet psmisc valgrind dracut systemd-libs systemd dracut-network libgudev1 \
+firewalld-filsystem python-firewall \
 systemd-sysv cryptsetup cryptsetup-libs expat libgcc libgomp libstdc++ java-1.7.0-openjdk "
 
 # EPICS dependencies

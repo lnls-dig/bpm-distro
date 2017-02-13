800d3f4ede61410f38bc8ba2c4d1e76f1e86d0e5
Lucas Russo <lerwys@gmail.com>
2017-02-13 16:43:31 -0200

    install-szip.sh: fix version typo

--- install-szip.sh
+++ install-szip.sh
@@ -3,13 +3,13 @@
 set -e
 set -x
 
-SZIP_VERSION=2.1.1
+SZIP_VERSION=2.1-1
 
 # Change to RPM directory
 cd kickstart_build/all_rpms
 
 # Download RPM
-wget http://springdale.math.ias.edu/data/puias/computational/7/x86_64//szip-$(SZIP_VERSION).sdl7.x86_64.rpm
+wget http://springdale.math.ias.edu/data/puias/computational/7/x86_64//szip-${SZIP_VERSION}.sdl7.x86_64.rpm
 
 # Remove re2c that may be already installed
 sudo yum remove -y szip

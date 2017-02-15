69d25da50ab292dff73f9b5e6d6c0092d2fd7322
Lucas Russo <lerwys@gmail.com>
2017-02-15 14:32:49 -0500

    {cleanup-oldkernels.sh,install-kernel.sh}: remove all kernels before installing

--- cleanup-oldkernels.sh
+++ cleanup-oldkernels.sh
@@ -5,6 +5,9 @@ set -x
 
 # Leave just the most recent kernel
 sudo package-cleanup -y --oldkernels --count=1
+sudo yum remove -y kernel-headers
+sudo yum remove -y kernel-tools
+sudo yum remove -y kernel-tools-libs
 
 # Make sure our all_rpms and Packages folder do not contain
 # any kernel pacakges

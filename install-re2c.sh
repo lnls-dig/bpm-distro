2415db1b38d4e84e802663a3db8f6e73fc2c8ea3
Lucas Russo <lerwys@gmail.com>
2017-11-06 17:03:27 -0200

    install-re2c.sh: update fedora .rpm location

--- install-re2c.sh
+++ install-re2c.sh
@@ -9,7 +9,7 @@ set -x
 cd kickstart_build/all_rpms
 
 # Download RPM
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/re2c-${RE2C_VERSION}.el6.x86_64.rpm
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/Packages/r/re2c-${RE2C_VERSION}.el6.x86_64.rpm
 
 # Remove re2c that may be already installed
 sudo yum remove -y re2c

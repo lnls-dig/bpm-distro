98d35807ca3ffd51ef4c104875335bc9c4a78364
Lucas Russo <lerwys@gmail.com>
2018-01-27 09:39:24 -0500

    install-kernel.sh: allow kernel downgrade
    This is important if the isntalled version is
    more recent than the specific version we requested
    in the repo-versions.sh script.

--- install-kernel.sh
+++ install-kernel.sh
@@ -21,12 +21,12 @@ wget ${KERNEL_DOWNLOAD_URL}/kernel-lt-tools-libs-${KERNEL_VERSION}.el7.elrepo.x8
 wget ${KERNEL_DOWNLOAD_URL}/kernel-lt-tools-libs-devel-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
 
 # Install them
-sudo rpm -Uvh --replacepkgs kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-devel-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-doc-${KERNEL_VERSION}.el7.elrepo.noarch.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-headers-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-tools-libs-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-tools-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
-sudo rpm -Uvh --replacepkgs kernel-lt-tools-libs-devel-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-devel-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-doc-${KERNEL_VERSION}.el7.elrepo.noarch.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-headers-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-tools-libs-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-tools-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
+sudo rpm -Uvh --oldpackage --replacepkgs kernel-lt-tools-libs-devel-${KERNEL_VERSION}.el7.elrepo.x86_64.rpm
 
 cd ../../
